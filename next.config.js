6e3a4ed6f3282305afc79f2eb6f11a66cf0e57ee
merunno <merunno92@gmail.com>
2021-10-03 21:48:33 +0900

    :sparkles: アイコンの追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   eslint: {
-    dirs: ["src/components/", "src/lib/", "src/pages/"],
+    dirs: ['src/components/', 'src/lib/', 'src/pages/'],
   },
-};
+  images: {
+    domains: ['twitter.com', 'pbs.twimg.com'],
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import { Box, chakra } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import IndexCss from '../../styles/index.module.css'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <Box>
-      <chakra.h1 color="tomato">Hello World!</chakra.h1>
+      <div className={IndexCss.Bodys}>
+        <div className={IndexCss.BodyIcon}>
+          <Image
+            src="https://pbs.twimg.com/profile_images/1443618769952149505/c5usFwMx_400x400.jpg"
+            alt={'merunno 冬のアイコン'}
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
     </Box>
   )
 }
