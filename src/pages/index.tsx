90fc5755c0cdfa8ba605ff784af6f833a5978c63
merunno <merunno92@gmail.com>
2021-11-04 20:03:30 +0900

    :sparkles: Channge: マイページを一つに

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { Box, Heading, Center } from '@chakra-ui/react'
+import { Box, Heading } from '@chakra-ui/react'
 import IndexCss from '../../styles/index.module.css'
 import React from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
-
-import Content_tab from '../component/_tab'
 
 export default function Home() {
   return (
@@ -24,10 +22,87 @@ export default function Home() {
           />
         </div>
         <Heading>Hello!</Heading>
-        <div className={IndexCss.contentBodys}>
-          <Center h="100%">
-            <Content_tab />
-          </Center>
+        <p>© merunno 2021</p>
+      </div>
+
+      <div className={IndexCss.Title}>
+        <div className={IndexCss.content}>名前</div>
+        <p>merunno</p>
+      </div>
+      <div className={IndexCss.Title}>
+        <div className={IndexCss.content}>年齢</div>
+        <p>16 * 2005/09/02</p>
+      </div>
+      <div className={IndexCss.Title}>
+        <div className={IndexCss.Title}>勉強中の言語</div>
+        <p>TypeScript / Java</p>
+      </div>
+      <div className={IndexCss.Title}>
+        <div className={IndexCss.content}>その他</div>
+        <p>JetBrains IDE. GitHub, Git, Markdownなど</p>
+      </div>
+      <div className={IndexCss.Title}>
+        <div className={IndexCss.content}>プロジェクト・参加事</div>
+        <div>
+          <p>◆ Battlefield Japan Community</p>
+          <p>→ 運営・開発者</p>
+        </div>
+        <div>
+          <p>◆ Azisaba Network</p>
+          <p>→ 運営・開発協力者(Afnw)</p>
+        </div>
+      </div>
+
+      <div className={IndexCss.Title}>
+        <div className={IndexCss.content}>連絡</div>
+        <div className={IndexCss.link}>
+          <p>連絡はこちらまでよろしくお願いします。</p>
+          <p>(Discord DMなどはフレンド以外切ってます。)</p>
+          <u>me@merunno.dev</u>
+        </div>
+        <div className={IndexCss.content}>ソースコード等</div>
+        <div className={IndexCss.link}>
+          <a
+            href="https://github.com/merunno/merunno.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>リポジトリ(merunno/merunno.dev)</u>
+          </a>
+        </div>
+        <div className={IndexCss.content}>リンク集</div>
+        <div className={IndexCss.link}>
+          <a
+            href="https://github.com/merunno"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>GitHub</u>
+          </a>
+          ・
+          <a
+            href="https://twitter.com/merunno"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>Twitter</u>
+          </a>
+          ・
+          <a
+            href="https://twitter.com/_meru92_"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>Twitter(鍵)</u>
+          </a>
+          ・
+          <a
+            href="https://keybase.io/merunno"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <u>KeyBase</u>
+          </a>
         </div>
       </div>
     </Box>
