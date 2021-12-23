af5580df85b52c801a0bdcf392904a1196291902
merunno <merunno92@gmail.com>
2021-12-24 00:22:41 +0900

    [add] OGPタグの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,11 @@ export default function Home() {
         <title>merunno.dev</title>
         <meta charSet="utf-8" />
         <meta content="merunnoのポートフォリオです。" name="description" />
+
+        {/* OGPタグ */}
+        <meta property={"og:url"} content={"https://www.merunno.dev/"} />
+        <meta property={"og:title"} content={"merunno のポートフォリオ"}/>
+        <meta property={"og:description"} content={"merunnoが作ったポートフォリオです。"}/>
       </Head>
       <div className={IndexCss.Bodys}>
         <div className={IndexCss.BodyIcon}>
