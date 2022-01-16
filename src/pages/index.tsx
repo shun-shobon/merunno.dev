0e8a88bc1d590225c0b09051e7ea5fcd76f4bf1c
merunno <merunno92@gmail.com>
2022-01-17 03:33:04 +0900

    add: ColorModeScriptの下準備

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Box, Center } from '@chakra-ui/react'
 import React from 'react'
 import Head from 'next/head'
+import { ColorModeScript } from '@chakra-ui/react'
 
 import Introduction from '../component/introduction.component'
 import SelfDescription from '../component/self_description.component'
 import PageButton from '../component/button.component'
 import RepositoryButton from '../component/github_link.component'
+import theme from '../config/theme'
 
 export default function Home() {
   return (
@@ -25,6 +27,7 @@ export default function Home() {
         <PageButton /> {/* ボタン */}
 
       <Center h={'200px'}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <RepositoryButton />
       </Center>
     </Box>
