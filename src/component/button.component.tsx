f5d5512fac74d54cb2993b9a13e56f9afed35568
merunno <merunno92@gmail.com>
2022-01-17 03:13:34 +0900

    add: プロジェクトURLを追加

--- src/component/button.component.tsx
+++ src/component/button.component.tsx
@@ -13,15 +13,20 @@ export default function ButtonC() {
             </Link>
           </Button>
           <Button colorScheme='teal' variant='ghost'>
-            <Link href='/projects'>
-              Projects
-            </Link>
-          </Button>
-          <Button colorScheme='teal' variant='ghost'>
             <Link href='/contects'>
               Contacts
             </Link>
           </Button>
+          {/* TODO: いつかポートフォリオ内に移植する : https://github.com/merunno/merunno.dev/issues/85 */}
+          <a
+            href="https://m92n.notion.site/80be3825bc264434a2799a5bb5586caf?v=2ae5b85320e549a2bae7c4496d53a48b"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button colorScheme='teal' variant='ghost' >
+              Projects
+            </Button>
+          </a>
         </Circle>
       </div>
     </main>
